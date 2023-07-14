1ec6e2bef3dbaa3a47978ab555a992a0fdd8d8ff
xyz123hhh <2690198301@qq.com>
2023-07-14 22:20:47 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue' 不用了（4.项目需求）
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+html,body,#app {
+  position:relative;
+  width:100%;
+  height:100%;
+  margin:0;
+}
+</style>
